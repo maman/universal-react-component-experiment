cb372bb76bfd9a01279f63c01da2a2ccc4df1ab7
Achmad Mahardi <achmad@mahardi.me>
2016-08-28 17:42:57 +0700

    Change directory organization

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import 'normalize.css';
+console.log(`
+Hi, thanks for taking interest on this experiment.
 
-console.log('No index specified');
+This packages *only* contains React Component that you can import
+to your app. To use, please do as following:
+
+import SSButton from 'universal-react/native/Button'; /** native */
+import SSButton from 'universal-react/web/Button'; /** web */
+
+If you need to look at the required/optional props, please look at
+'src/componentProps' directory.
+`);
